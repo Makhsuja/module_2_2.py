dc3fc4cb4585378277254bf6954c3386f5e14e9b
Makhsuja <makhsuja18@gmail.com>
2024-11-05 20:04:48 +0300

    Update module_2_2.py.py

--- module_2_2.py.py
+++ module_2_2.py.py
@@ -1,7 +1,7 @@
 first = int(input('Введите число: '))
 second = int(input('Введите число: '))
 third = int(input('Введите число: '))
-if first == second and second == first and first == third and third == first and second == first and first == second:
+if first == second and first == third and second == third:
     print(3)
 elif first == second and second == first:
     print(2)
@@ -9,11 +9,5 @@ elif first == third and third == first:
     print(2)
 elif second == third and third == second:
     print(2)
-elif first == second:
-    print(1)
-elif second == third:
-    print(1)
-elif third == first:
-    print(1)
 else:
     print(0)
